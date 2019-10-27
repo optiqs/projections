dca861d289d2333aa49ec80734c9e2d568b9554c
Thomas Monte <thomasmont3@gmail.com>
2019-10-27 11:12:58 -0600

    Add build, lint, type-checking setup

--- src/projection.test.ts
+++ src/projection.test.ts
@@ -1,33 +1,33 @@
-import { Projection } from "./projection";
+import {Projection} from './projection'
 
-test("Projection.fromProp: drills down to an objects prop", () => {
-  type S = { a: string };
-  const s: S = { a: "A" };
-  const projection = Projection.fromProp<S>()("a");
-  const expected = s.a;
-  const actual = projection.get(s);
-  expect(actual).toEqual(expected);
-});
+test('Projection.fromProp: drills down to an objects prop', () => {
+  type S = {a: string}
+  const s: S = {a: 'A'}
+  const projection = Projection.fromProp<S>()('a')
+  const expected = s.a
+  const actual = projection.get(s)
+  expect(actual).toEqual(expected)
+})
 
-test("Projection.map: obeys identity law", () => {
-  type S = { a: string };
-  const s: S = { a: "A" };
-  const projection = Projection.fromProp<S>()("a");
-  const expected = s.a;
-  const actual = projection.map(a => a).get(s);
-  expect(actual).toEqual(expected);
-});
+test('Projection.map: obeys identity law', () => {
+  type S = {a: string}
+  const s: S = {a: 'A'}
+  const projection = Projection.fromProp<S>()('a')
+  const expected = s.a
+  const actual = projection.map(a => a).get(s)
+  expect(actual).toEqual(expected)
+})
 
-test("Projection.map: obeys composition law", () => {
-  type S = { a: string };
-  const s: S = { a: "A" };
-  const projection = Projection.fromProp<S>()("a");
-  const f = (v: string) => `f(${v})`;
-  const g = (v: string) => `g(${v})`;
+test('Projection.map: obeys composition law', () => {
+  type S = {a: string}
+  const s: S = {a: 'A'}
+  const projection = Projection.fromProp<S>()('a')
+  const f = (v: string): string => `f(${v})`
+  const g = (v: string): string => `g(${v})`
   const split = projection
     .map(f)
     .map(g)
-    .get(s);
-  const composed = projection.map(v => g(f(v))).get(s);
-  expect(split).toEqual(composed);
-});
+    .get(s)
+  const composed = projection.map(v => g(f(v))).get(s)
+  expect(split).toEqual(composed)
+})
--- src/projection.ts
+++ src/projection.ts
@@ -1,9 +1,9 @@
-import { Lens } from "monocle-ts";
-import { flow } from "fp-ts/lib/function";
+import {flow} from 'fp-ts/lib/function'
+import {Lens} from 'monocle-ts'
 
-declare module "monocle-ts" {
+declare module 'monocle-ts' {
   interface Lens<S, A> {
-    asProjection(): Projection<S, A>;
+    asProjection(): Projection<S, A>
   }
 }
 
@@ -16,7 +16,7 @@ export interface ProjectionFromPath<S> {
     K5 extends keyof S[K1][K2][K3][K4]
   >(
     path: [K1, K2, K3, K4, K5]
-  ): Projection<S, S[K1][K2][K3][K4][K5]>;
+  ): Projection<S, S[K1][K2][K3][K4][K5]>
   <
     K1 extends keyof S,
     K2 extends keyof S[K1],
@@ -24,111 +24,103 @@ export interface ProjectionFromPath<S> {
     K4 extends keyof S[K1][K2][K3]
   >(
     path: [K1, K2, K3, K4]
-  ): Projection<S, S[K1][K2][K3][K4]>;
+  ): Projection<S, S[K1][K2][K3][K4]>
   <K1 extends keyof S, K2 extends keyof S[K1], K3 extends keyof S[K1][K2]>(
     path: [K1, K2, K3]
-  ): Projection<S, S[K1][K2][K3]>;
-  <K1 extends keyof S, K2 extends keyof S[K1]>(path: [K1, K2]): Projection<
-    S,
-    S[K1][K2]
-  >;
-  <K1 extends keyof S>(path: [K1]): Projection<S, S[K1]>;
+  ): Projection<S, S[K1][K2][K3]>
+  <K1 extends keyof S, K2 extends keyof S[K1]>(path: [K1, K2]): Projection<S, S[K1][K2]>
+  <K1 extends keyof S>(path: [K1]): Projection<S, S[K1]>
 }
 
 export class Projection<S, A> {
-  private lens: Lens<S, A>;
-
-  static fromProp<S>() {
-    return <P extends keyof S>(prop: P) =>
-      Lens.fromProp<S>()(prop).asProjection();
-  }
-
-  static fromProps<S>() {
-    return <P extends keyof S>(props: P[]) =>
-      Lens.fromProps<S>()(props).asProjection();
-  }
-
-  static fromPath<S>() {
-    return ((path: any) =>
-      Lens.fromPath<S>()(path).asProjection()) as ProjectionFromPath<S>;
-  }
-
-  static fromNullableProp<S>() {
-    return <A extends S[K], K extends keyof S>(k: K, defaultValue: A) =>
-      Lens.fromNullableProp<S>()(k, defaultValue).asProjection();
-  }
+  private readonly lens: Lens<S, A>
 
   constructor(getter: (s: S) => A) {
-    this.lens = new Lens(getter, () => s => s);
+    const id: (_: S) => S = s => s
+    const setter: () => typeof id = () => id
+    this.lens = new Lens(getter, setter)
   }
 
-  compose<B>(sb: Projection<A, B>): Projection<S, B> {
-    return this.lens.compose(sb.lens).asProjection();
+  public compose<B>(sb: Projection<A, B>): Projection<S, B> {
+    return this.lens.compose(sb.lens).asProjection()
   }
 
-  composeLens<B>(sb: Lens<A, B>): Projection<S, B> {
-    return this.compose(sb.asProjection());
+  public composeLens<B>(sb: Lens<A, B>): Projection<S, B> {
+    return this.compose(sb.asProjection())
   }
 
-  combine<B, R>(sb: Projection<S, B>, st: (a: A, b: B) => R): Projection<S, R>;
-  combine<B, C, R>(
+  public combine<B, R>(sb: Projection<S, B>, st: (a: A, b: B) => R): Projection<S, R>
+  public combine<B, C, R>(
     ss: [Projection<S, B>, Projection<S, C>],
     st: (a: A, b: B, c: C) => R
-  ): Projection<S, R>;
-  combine<B, C, D, R>(
+  ): Projection<S, R>
+  public combine<B, C, D, R>(
     ss: [Projection<S, B>, Projection<S, C>, Projection<S, D>],
     st: (a: A, b: B, c: C, d: D) => R
-  ): Projection<S, R>;
-  combine<B, C, D, E, R>(
-    ss: [
-      Projection<S, B>,
-      Projection<S, C>,
-      Projection<S, D>,
-      Projection<S, E>
-    ],
+  ): Projection<S, R>
+  public combine<B, C, D, E, R>(
+    ss: [Projection<S, B>, Projection<S, C>, Projection<S, D>, Projection<S, E>],
     st: (a: A, b: B, c: C, d: D, e: E) => R
-  ): Projection<S, R>;
-  combine(sb: any, st: any) {
+  ): Projection<S, R>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public combine<R>(sb: any, st: any): Projection<S, R> {
     return new Projection((s: S) => {
-      const args = Array.isArray(sb) ? sb.map(sbv => sbv.get(s)) : [sb.get(s)];
-      return st(this.lens.get(s), ...args);
-    });
+      const args = Array.isArray(sb) ? sb.map(sbv => sbv.get(s)) : [sb.get(s)]
+      return st(this.lens.get(s), ...args)
+    })
   }
 
-  combineLens<B, R>(sb: Lens<S, B>, st: (a: A, b: B) => R): Projection<S, R>;
-  combineLens<B, C, R>(
+  public combineLens<B, R>(sb: Lens<S, B>, st: (a: A, b: B) => R): Projection<S, R>
+  public combineLens<B, C, R>(
     ss: [Lens<S, B>, Lens<S, C>],
     st: (a: A, b: B, c: C) => R
-  ): Projection<S, R>;
-  combineLens<B, C, D, R>(
+  ): Projection<S, R>
+  public combineLens<B, C, D, R>(
     ss: [Lens<S, B>, Lens<S, C>, Lens<S, D>],
     st: (a: A, b: B, c: C, d: D) => R
-  ): Projection<S, R>;
-  combineLens<B, C, D, E, R>(
+  ): Projection<S, R>
+  public combineLens<B, C, D, E, R>(
     ss: [Lens<S, B>, Lens<S, C>, Lens<S, D>, Lens<S, E>],
     st: (a: A, b: B, c: C, d: D, e: E) => R
-  ): Projection<S, R>;
-  combineLens(sb: any, st: any) {
-    const args = Array.isArray(sb)
-      ? sb.map(sbv => sbv.asProjection())
-      : sb.asProjection();
-    return this.combine(args, st);
+  ): Projection<S, R>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public combineLens<R>(sb: any, st: any): Projection<S, R> {
+    const args = Array.isArray(sb) ? sb.map(sbv => sbv.asProjection()) : sb.asProjection()
+    return this.combine(args, st)
   }
 
-  map<B>(f: (a: A) => B) {
+  public map<B>(f: (a: A) => B): Projection<S, B> {
     return new Projection(
       flow(
         this.lens.get,
         f
       )
-    );
+    )
   }
 
-  get(s: S) {
-    return this.lens.get(s);
+  public get(s: S): A {
+    return this.lens.get(s)
+  }
+
+  public static fromProp<S>() {
+    return <P extends keyof S>(prop: P) => Lens.fromProp<S>()(prop).asProjection()
+  }
+
+  public static fromProps<S>() {
+    return <P extends keyof S>(props: P[]) => Lens.fromProps<S>()(props).asProjection()
+  }
+
+  public static fromPath<S>() {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return ((path: any) => Lens.fromPath<S>()(path).asProjection()) as ProjectionFromPath<S>
+  }
+
+  public static fromNullableProp<S>() {
+    return <A extends S[K], K extends keyof S>(k: K, defaultValue: A) =>
+      Lens.fromNullableProp<S>()(k, defaultValue).asProjection()
   }
 }
 
 Lens.prototype.asProjection = function() {
-  return new Projection(this.get);
-};
+  return new Projection(this.get)
+}
